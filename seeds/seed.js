edc0c91ef1d28403046ade4ea36797e5d8b20d4e
SindidR <sindidreedh@gmail.com>
2022-11-08 20:52:53 -0500

    seed-js-done

---
+++ seeds/seed.js
@@ -0,0 +1,30 @@
+const sequelize = require('../config/connection');
+const { User, Blog } = require('../models');
+
+const userData = require('./userData.json');
+const blogData = require('./blogData.json');
+
+const seedDatabase = async () => {
+    await sequelize.sync({ force: true });
+
+    const users = await User.bulkCreate(userData, {
+        individualHooks: true,
+        returning: true,
+    });
+
+    const blogs = await Blog.bulkCreate(blogData, {
+        individualHooks: true,
+        returning: true,
+    });
+
+    for (const project of blogData) {
+        await Blog.create({
+            ...blogs,
+            user_id: users[Math.floor(Math.random() * users.length)].id,
+        });
+    }
+
+    process.exit(0);
+};
+
+seedDatabase();
